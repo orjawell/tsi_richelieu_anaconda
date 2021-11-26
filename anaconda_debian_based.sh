e053d724c5d7941c3d8655112f185b43262115ce
Orjawell <69430732+orjawell@users.noreply.github.com>
2021-11-26 20:20:33 +0100

    Add files via upload

---
+++ anaconda_debian_based.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+echo "Script auto - Telechargement des logiciels neccesaires pour la Prepa TSI"
+echo " "
+echo " "
+echo "Script cree par Akram Shal pour les utilisateurs de Linux en Prepa TSI au Lycée Richelieu - Ruel Malmaison"
+
+sleep 100000 &
+
+echo "Ce script va installer automatiquement les logiciels suivants :"
+echo  "Anaconda 3 Full Package with Spyder 5.1.5"
+echo  "Automatic Update for Fedora Linux"
+echo " "
+echo " "
+echo "Attention, cette version pour Ubuntu / PopOS / Autre OS basé a partir de Debian, un redémarrage à la fin du script sera effectué automatiquement !"
+
+sleep 10 &
+echo "etes vous sur pour l'installation de l'intégralité des logiciels ? (temps estimé : 20 min) [o/N]"
+read yes
+if [[ ($yes == o) ]]; then 
+echo "Telechargement en cours"
+curl -O https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh
+sleep 5 &
+echo "Merci de bien vouloir suivre les instructions de l'installateur"
+sleep 30 &
+bash Anaconda3-2021.11-Linux-x86_64.sh
+sleep 10 &
+echo "Lancement des MàJ"
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
+sudo reboot
+sleep 10 &
+echo "Téléchargement terminé, Bienvenue sur votre environnement de travail."
+rm  Anaconda3-2021.11-Linux-x86_64.sh
+fi
+
